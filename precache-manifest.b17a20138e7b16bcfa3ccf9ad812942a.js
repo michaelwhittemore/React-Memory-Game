eb874234314fc041e001cba847869b899a5adb52
michaelwhittemore <michaelwhittemore@gmail.com>
2019-02-17 17:48:00 -0800

    Updates

---
+++ precache-manifest.b17a20138e7b16bcfa3ccf9ad812942a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "f13278d95ece8cc183f2",
+    "url": "/React-Memory-Game/static/js/runtime~main.f13278d9.js"
+  },
+  {
+    "revision": "61966b5e8cde0781c812",
+    "url": "/React-Memory-Game/static/js/main.61966b5e.chunk.js"
+  },
+  {
+    "revision": "0f0f1ed395de9ee32ca5",
+    "url": "/React-Memory-Game/static/js/2.0f0f1ed3.chunk.js"
+  },
+  {
+    "revision": "be3301f1d93e337b3e3576d42cabbc73",
+    "url": "/React-Memory-Game/index.html"
+  }
+];
